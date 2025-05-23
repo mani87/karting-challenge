1a1baadd6bc8bb5acc99c645908772c4fcc7eea2
mani87 <mani.kumar8721@gmail.com>
2025-05-23 14:23:49 +0530

    Add API implementation

--- internal/handlers/order.go
+++ internal/handlers/order.go
@@ -2,9 +2,39 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"karting-challenge/internal/models"
 	"net/http"
 )
 
 func Order(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Order Placed"})
+	var req models.OrderReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"message": "Invalid order"})
+		return
+	}
+
+	// todo: validate promo code
+
+	// Create Order
+	order := models.Order{
+		ID:        uuid.New().String(),
+		Items:     req.Items,
+		Discounts: 0.0,
+		Total:     0.0,
+		Products:  []models.Product{},
+	}
+
+	for _, item := range req.Items {
+		for _, p := range models.Products {
+			if p.ID == item.ProductID {
+				order.Products = append(order.Products, p)
+				order.Total += float64(item.Quantity) * p.Price
+			}
+		}
+	}
+
+	// todo: Take care of discount here
+
+	c.JSON(http.StatusOK, order)
 }
